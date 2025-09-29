45920d2544892476e4e7465681bc90b6812c2802
니꼬 <nico@nomadcoders.co>
2025-09-29 18:54:55 +0200

    17.5 Testing AI Nodes

--- workflow-testing/tests.py
+++ workflow-testing/tests.py
@@ -1,21 +1,25 @@
+import dotenv
+
+dotenv.load_dotenv()
+
 import pytest
 from main import graph
 
 
 @pytest.mark.parametrize(
-    "email, expected_category, expected_score",
+    "email, expected_category, min_score, max_score",
     [
-        ("this is urgent!", "urgent", 10),
-        ("i wanna talk to you", "normal", 5),
-        ("i have an offer for you", "spam", 1),
+        ("this is urgent!", "urgent", 8, 10),
+        ("i wanna talk to you", "normal", 4, 7),
+        ("i have an offer for you", "spam", 1, 3),
     ],
 )
-def test_full_graph(email, expected_category, expected_score):
+def test_full_graph(email, expected_category, min_score, max_score):
 
     result = graph.invoke({"email": email}, config={"configurable": {"thread_id": "1"}})
 
     assert result["category"] == expected_category
-    assert result["priority_score"] == expected_score
+    assert min_score <= result["priority_score"] <= max_score
 
 
 def test_individual_nodes():
@@ -27,15 +31,17 @@ def test_individual_nodes():
 
     # assing_priority
 
-    result = graph.nodes["assing_priority"].invoke({"category": "spam"})
+    result = graph.nodes["assing_priority"].invoke(
+        {"category": "spam", "email": "buy this pot."}
+    )
 
-    assert result["priority_score"] == 1
+    assert 1 <= result["priority_score"] <= 3
 
     # draft_response
 
-    result = graph.nodes["draft_response"].invoke({"category": "spam"})
+    # result = graph.nodes["draft_response"].invoke({"category": "spam"})
 
-    assert "Go away" in result["response"]
+    # assert "Go away" in result["response"]
 
 
 def test_partial_execution():
@@ -63,4 +69,4 @@ def test_partial_execution():
         interrupt_after="draft_response",
     )
 
-    assert result["priority_score"] == 1
+    assert 1 <= result["priority_score"] <= 3
