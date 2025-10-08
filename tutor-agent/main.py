5c2dfa902c057d19b828f4783354fd19008906df
니꼬 <nico@nomadcoders.co>
2025-10-08 17:34:39 +0200

    19.2 Feynman Agent

--- tutor-agent/main.py
+++ tutor-agent/main.py
@@ -5,17 +5,42 @@ load_dotenv()
 from langgraph.graph import START, END, StateGraph, MessagesState
 
 from agents.classification_agent import classification_agent
+from agents.teacher_agent import teacher_agent
+from agents.feynman_agent import feynman_agent
 
 
 class TutorState(MessagesState):
-    pass
+    current_agent: str
+
+
+def router_check(state: TutorState):
+    current_agent = state.get("current_agent", "classification_agent")
+    return current_agent
 
 
 graph_builder = StateGraph(TutorState)
 
-graph_builder.add_node("classification_agent", classification_agent)
+graph_builder.add_node(
+    "classification_agent",
+    classification_agent,
+    destinations=(
+        "quiz_agent",
+        "teacher_agent",
+        "feynman_agent",
+    ),
+)
+graph_builder.add_node("teacher_agent", teacher_agent)
+graph_builder.add_node("feynman_agent", feynman_agent)
 
-graph_builder.add_edge(START, "classification_agent")
+graph_builder.add_conditional_edges(
+    START,
+    router_check,
+    [
+        "teacher_agent",
+        "feynman_agent",
+        "classification_agent",
+    ],
+)
 graph_builder.add_edge("classification_agent", END)
 
 graph = graph_builder.compile()
