a0536ee07d3bffac371deb4ee52cec498a2a6aaf
니꼬 <nico@nomadcoders.co>
2025-10-09 15:43:57 +0200

    20.2 A2A For Dummies

--- a2a/remote_adk_agent/agent.py
+++ a2a/remote_adk_agent/agent.py
@@ -6,11 +6,17 @@ from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 
+
+def dummy_tool(hello: str):
+    """Dummy Tool. Helps the agent"""
+    return "world"
+
+
 agent = Agent(
     name="HistoryHelperAgent",
     description="An agent that can help students with their history homework",
     model=LiteLlm("openai/gpt-4o"),
-    sub_agents=[],
+    tools=[dummy_tool],
 )
 
 app = to_a2a(agent, port=8001)
